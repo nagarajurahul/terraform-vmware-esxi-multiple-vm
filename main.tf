63da73f80aec0d4a67b84d11ad44519b56926f7d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-16 20:39:57 -0500

    modified module layout

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 module "vm"{
-  source = "git::https://github.com/nagarajurahul/terraform-vmware-esxi-single-vm.git?ref=v1.2.0"
+  source = "git::https://github.com/nagarajurahul/terraform-vmware-esxi-vm-module.git?ref=v1.3.0"
 
   for_each = var.vms
 
   esxi_hostname    = each.value.esxi_hostname
-  esxi_hostport    = "22"
-  esxi_hostssl     = "443"
   esxi_username    = each.value.esxi_username
   esxi_password    = each.value.esxi_password
+
   disk_store       = each.value.disk_store
   virtual_network  = each.value.virtual_network
   vm_hostname      = each.value.vm_hostname
