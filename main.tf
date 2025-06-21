84445e8b063a2f844dc279a97168cb01351e7de5
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-21 16:02:43 -0500

    update vm module source reference to v3.0.4

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "esxi" {
 }
 
 module "vm"{
-  source = "git::https://github.com/nagarajurahul/terraform-vmware-esxi-vm-module.git?ref=v3.0.3"
+  source = "git::https://github.com/nagarajurahul/terraform-vmware-esxi-vm-module.git?ref=v3.0.4"
 
   for_each = var.vms
 
